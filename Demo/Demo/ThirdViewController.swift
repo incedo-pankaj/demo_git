86d8c61b5072912cee753dd079f9d51c6ec59898
Pankaj <pankaj.jangid@incedoinc.com>
2021-06-29 10:44:20 +0530

    upload third VC

---
+++ Demo/Demo/ThirdViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdViewController.swift
+//  Demo
+//
+//  Created by Pankaj Jangid on 29/06/21.
+//
+
+import SwiftUI
+
+struct ThirdViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdViewController()
+    }
+}
